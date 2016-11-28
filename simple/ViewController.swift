d64c7ccc72c226dc47f3f225c9ced74925f39baa
I857853 <vishwas.mukund@sap.com>
2016-11-27 19:31:04 -0800

    Initial Commit

---
+++ simple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple
+//
+//  Created by Mukund, Vishwas on 11/27/16.
+//  Copyright © 2016 Mukund, Vishwas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
